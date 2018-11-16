79a1257e3b170ecbf8d87058bb72b061be056036
Gabbs1110 <gabb8091@gmail.com>
2018-11-15 22:23:35 -0200

    4 verbos http

---
+++ RestAspNetCore/apiRestPersons/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiRestPersons.Model
+{
+    public class Person
+    {
+        public long id { get;set; }
+        public string primeiroNome { get;set ;}
+        public string segundoNome { get;set ;}
+        public string Endereço { get;set ;}
+        public string Genero { get;set ;}
+    }
+}
